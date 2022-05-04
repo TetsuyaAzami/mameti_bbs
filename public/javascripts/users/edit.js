4662e90ccffbc847884cb095e0fb93a93e4ace96
Tetsuya Azami <zqianjian35@gmail.com>
2022-05-05 08:58:49 +0900

    ユーザプロフィールの編集画面にて、画像選択時に非同期で画像が表示されるようにしました

--- public/javascripts/users/edit.js
+++ public/javascripts/users/edit.js
@@ -2,26 +2,59 @@
   const fileSizeLimit = 1024 * 1024 * 2;
   const $profileImgInput = document.getElementById("profileImgInput");
   const $profileImgError = document.getElementById("profileImgError");
+  const $preview = document.getElementById("preview");
   const $updateButton = document.getElementById("updateButton");
 
-  //ファイルサイズを2MBに制限。
-  const fileSizeValidation = () => {
-    // 初期化処理
+  // 初期化処理
+  const initFileHandler = () => {
     $profileImgError.innerText = "";
     $updateButton.disabled = false;
+    while ($preview.firstChild) {
+      $preview.removeChild($preview.firstChild);
+    }
+  };
 
+  //ファイルサイズを2MBに制限。
+  const fileSizeCheck = (file) => {
+    const fileSize = file.size;
+    if (fileSize > fileSizeLimit) {
+      $profileImgError.innerText =
+        "ファイルサイズが大きすぎます。2MB以内にしてください";
+      $updateButton.disabled = true;
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  //プレビューの表示
+  const previewFile = (file) => {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const imageUrl = e.target.result;
+      const img = document.createElement("img");
+      img.src = imageUrl;
+      img.classList.add("profile-img-lg");
+      $preview.appendChild(img);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  //各種ファイル処理
+  const fileHandler = () => {
+    initFileHandler();
     const files = $profileImgInput.files;
     Array.from(files).map((file) => {
-      const fileSize = file.size;
-      if (fileSize > fileSizeLimit) {
-        $profileImgError.innerText =
-          "ファイルサイズが大きすぎます。2MB以内にしてください";
-        $updateButton.disabled = true;
+      //ファイルサイズが適正なら画像のプレビューを表示
+      if (fileSizeCheck(file) == true) {
+        previewFile(file);
       }
     });
   };
 
   $profileImgInput.addEventListener("change", () => {
-    fileSizeValidation();
+    fileHandler();
   });
 })();
