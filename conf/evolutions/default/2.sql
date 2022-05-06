b3613ed2794bf381f6cd6988374289ae48ffb7ca
Tetsuya Azami <zqianjian35@gmail.com>
2022-05-06 10:43:18 +0900

    マイページにてユーザのプロフィール画像を表示できるようにしました

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,9 +1,10 @@
 --!Ups
 INSERT INTO departments (name)VALUES('フロントエンド'),('バックエンド'),('クラウド');
 
-INSERT INTO users (name, email, password, birthday, introduce, profile_img, department_id) VALUES('user1','email1@example.com', 'password1', '2000-11-11','user1です', 'img1',1);
-INSERT INTO users (name, email, password, birthday, introduce, profile_img, department_id) VALUES('user2','email2@example.com', 'password2', '2000-11-12','user2です', 'img2',2);
-INSERT INTO users (name, email, password, birthday, introduce, profile_img, department_id) VALUES('user3','email3@example.com', 'password3', '2000-11-13','user3です', 'img3',3);
+INSERT INTO users (name, email, password, birthday, introduce, profile_img, department_id) VALUES('user1','email1@example.com', 'password1', '2000-11-11','user1です', 'email1@example.com.png',1);
+INSERT INTO users (name, email, password, birthday, introduce, profile_img, department_id) VALUES('user2','email2@example.com', 'password2', '2000-11-12','user2です', 'email2@example.com.jpg',2);
+INSERT INTO users (name, email, password, birthday, introduce, profile_img, department_id) VALUES('user3','email3@example.com', 'password3', '2000-11-13','user3です', null,3);
+INSERT INTO users (name, email, password, birthday, introduce, profile_img, department_id) VALUES('user4','email4@example.com', 'password4', null, null, null, 3);
 
 INSERT INTO posts (content,user_id,posted_at) VALUES ('user1投稿1', 1, NOW());
 INSERT INTO posts (content,user_id,posted_at) VALUES ('user1投稿2', 1, NOW());
