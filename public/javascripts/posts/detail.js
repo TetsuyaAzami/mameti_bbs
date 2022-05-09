2eaa5df55c4bc773152603d0769b3ffcc260e642
Tetsuya Azami <zqianjian35@gmail.com>
2022-05-10 06:41:41 +0900

    axios共通部分のリファクタリングと、投稿詳細画面でのいいねinsert,deleteの実装

--- public/javascripts/posts/detail.js
+++ public/javascripts/posts/detail.js
@@ -5,7 +5,7 @@
   const $commentList = document.getElementById("commentList");
   const $commentCount = document.getElementById("commentCount");
 
-  const commentCsrfToken = $commentInsertForm.children[0].value;
+  const csrfToken = $commentInsertForm.children[0].value;
   const commentPostId = document.getElementById("postId").value;
 
   // コメント投稿処理
@@ -21,7 +21,7 @@
         {
           headers: {
             "Content-Type": "application/json",
-            "Csrf-Token": commentCsrfToken,
+            "Csrf-Token": csrfToken,
           },
         }
       )
@@ -48,5 +48,35 @@
           $commentContentInput
         );
       });
+  });
+
+  // いいねinsert, delete
+  const $likeInfoList = document.getElementsByClassName("likeInfo");
+  Array.from($likeInfoList).map((likeInfo) => {
+    const $heart = likeInfo.children[0];
+    const $heartCountSpan = likeInfo.children[1];
+
+    //いいねinsert 成功パターン
+    const insertSuccessFunction = (response) => {
+      $heartCountSpan.innerText = response.data;
+      $heart.classList.remove("like-btn");
+      $heart.classList.add("unlike-btn");
+    };
+    //いいねdelete 成功パターン
+    const deleteSuccessFunction = (response) => {
+      $heartCountSpan.innerText = response.data;
+      $heart.classList.remove("unlike-btn");
+      $heart.classList.add("like-btn");
+    };
+
+    $heart.addEventListener("click", (e) => {
+      const likePostId = e.target.dataset.postId;
+      const isInsertButton = e.target.classList.contains("like-btn");
+      if (isInsertButton) {
+        insertLikeManipulator(likePostId, csrfToken, insertSuccessFunction);
+      } else {
+        deleteLikeManipulator(likePostId, csrfToken, deleteSuccessFunction);
+      }
+    });
   });
 })();
