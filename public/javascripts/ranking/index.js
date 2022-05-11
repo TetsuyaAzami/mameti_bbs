1b51ad27b1480b390bfbcca47b41f441ae66ae69
Tetsuya Azami <zqianjian35@gmail.com>
2022-05-12 06:11:11 +0900

    アクティブなタブを検索URLによって動的に変更するようにしました

--- public/javascripts/ranking/index.js
+++ public/javascripts/ranking/index.js
@@ -1,6 +1,30 @@
 (function () {
-  const csrfToken = document.forms.signOutForm.children[0].value;
+  // 部門activeタブ判定
+  const url = new URL(location.href);
+  const search = url.search;
+  const decodedsearch = decodeURI(search);
+  // クエリパラメータdepartmentのvalueを抽出
+  const queryDepartmentIndex = decodedsearch.indexOf("department");
+  const ampersandIndex = decodedsearch.indexOf("&");
+  const selectedDepartment = decodedsearch.substring(
+    queryDepartmentIndex + "department".length + 1,
+    ampersandIndex
+  );
+  const navLinks = document.getElementsByClassName("nav-link");
+  // タブ初期化処理
+  Array.from(navLinks).forEach((navLink) => navLink.classList.remove("active"));
+  if (selectedDepartment == "フロントエンド") {
+    navLinks[1].classList.add("active");
+  } else if (selectedDepartment == "バックエンド") {
+    navLinks[2].classList.add("active");
+  } else if (selectedDepartment == "クラウド") {
+    navLinks[3].classList.add("active");
+  } else {
+    navLinks[0].classList.add("active");
+  }
+
   // いいねinsert, delete
+  const csrfToken = document.forms.signOutForm.children[0].value;
   const $likeInfoList = document.getElementsByClassName("likeInfo");
   Array.from($likeInfoList).map((likeInfo) => {
     const $heart = likeInfo.children[0];
