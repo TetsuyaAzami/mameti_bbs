e1e166ea5e13f9292812acb9ddc9ed0ac5aade39
Tetsuya Azami <zqianjian35@gmail.com>
2022-05-16 09:23:55 +0900

    MYSQLに移行しました

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,40 +1,41 @@
 --!Ups
-CREATE TABLE departments(
-department_id serial PRIMARY KEY,
-name VARCHAR(30) NOT NULL UNIQUE
+CREATE TABLE departments (
+	department_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(30) NOT NULL UNIQUE
 );
-CREATE INDEX ON departments(name);
+CREATE INDEX department_name_idx ON departments(name);
 
-CREATE TABLE users(
-user_id serial PRIMARY KEY,
-name varchar(128) NOT NULL,
-email varchar(254) NOT NULL UNIQUE ,
-password varchar(128) CHECK (LENGTH(password) >= 8),
-birthday date CHECK(birthday < CURRENT_DATE),
-introduce varchar(200),
-profile_img text,
-department_id integer NOT NULL REFERENCES departments(department_id)
-);
-
-CREATE TABLE posts(
-post_id serial PRIMARY KEY,
-content varchar(140) NOT NULL,
-user_id integer NOT NULL REFERENCES users(user_id),
-posted_at timestamp NOT NULL
+CREATE TABLE users (
+	user_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name varchar(128) NOT NULL,
+	email varchar(254) NOT NULL UNIQUE,
+	password varchar(128) CONSTRAINT `password_check` CHECK ((CHAR_LENGTH(`password`) >= 8)),
+	birthday date,
+	introduce varchar(200),
+	profile_img text,
+	department_id int NOT NULL REFERENCES departments (department_id)
 );
 
-CREATE TABLE comments(
-comment_id serial PRIMARY KEY,
-user_id integer REFERENCES users(user_id),
-post_id integer REFERENCES posts(post_id) ON DELETE CASCADE,
-content varchar(140) NOT NULL,
-commented_at timestamp NOT NULL
+
+CREATE TABLE posts (
+	post_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	content varchar(140) NOT NULL,
+	user_id int NOT NULL REFERENCES users (user_id),
+	posted_at timestamp NOT NULL
 );
 
-CREATE TABLE likes(
-like_id serial PRIMARY KEY,
-user_id integer REFERENCES users(user_id),
-post_id integer REFERENCES posts(post_id) ON DELETE CASCADE,
+CREATE TABLE comments (
+	comment_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id int REFERENCES users (user_id),
+	post_id int REFERENCES posts (post_id) ON DELETE CASCADE,
+	content varchar(140) NOT NULL,
+	commented_at timestamp NOT NULL
+);
+
+CREATE TABLE likes (
+	like_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id int REFERENCES users (user_id),
+	post_id int REFERENCES posts (post_id) ON DELETE CASCADE,
 	UNIQUE (user_id, post_id)
 );
 
